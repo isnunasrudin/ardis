172893cda6f2a736f1d0023184e5b0f1db5b269b
Isnu Nasrudin <isnunas@gmail.com>
2022-06-25 14:01:18 +0700

    Merapikan Login

--- app/views/fyea.php
+++ app/views/fyea.php
@@ -1,13 +1,13 @@
 <div class="card" style="width: 18rem;">
   <div class="card-body">
-  <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email address/NISN</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Masukan NISN Anda">
-</div>
-<div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label">Password/Tanggal Lahir</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
-</div>
-<button type="button" class="btn btn-primary mx-auto d-block">LOGIN</button>
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Email address/NISN</label>
+      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Masukan NISN Anda">
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">Password/Tanggal Lahir</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+    </div>
+    <button type="button" class="btn btn-primary mx-auto d-block">LOGIN</button>
   </div>
 </div>
